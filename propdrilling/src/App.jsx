8a455d584d08ba0f8b75b44fb9eb6a6771137c2e
Rishabh Shukla <rishabh.489.c@gmail.com>
2024-06-17 12:17:45 +0530

    intern'

--- propdrilling/src/App.jsx
+++ propdrilling/src/App.jsx
@@ -8,28 +8,36 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <div>
+      <Count count={count} setCount={setCount}></Count>
+      
+     </div>
     </>
   )
+}
+
+function Count({count,setCount}){
+  return <div>
+    <CountRendered count={count}/>
+    <Buttons count={count} setCount={setCount}></Buttons>
+  </div>
+}
+
+function CountRendered({count}){
+  return <div>
+    {count}
+  </div>
+}
+
+function Buttons({count,setCount}){
+ return <div>
+  <button onClick={()=>{
+    setCount(count+1)
+  }}>Increase</button>
+  <button onClick={()=>{
+    setCount(count -1)
+  }}>Decrease</button>
+ </div>
 }
 
 export default App
